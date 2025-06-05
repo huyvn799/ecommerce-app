6c47e3ad58edc97a03245b6d5d0c89770b90b9a8
huyvn799 <quochuy2001.kb@gmail.com>
2025-06-06 02:23:07 +0700

    How to use DTO

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -1,22 +1,39 @@
 package com.codewithmosh.store.controllers;
 
+import com.codewithmosh.store.dtos.UserDto;
 import com.codewithmosh.store.entities.User;
 import com.codewithmosh.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @AllArgsConstructor
+@RequestMapping("/users")
 public class UserController {
-    private final UserRepository userRepository;
+    private final UserRepository<User> userRepository;
 
-    @GetMapping("/users")
+    @GetMapping()
     // method: GET
-    public Iterable<User> getAllUsers() {
-        return userRepository.findAll();
+    public Iterable<UserDto> getAllUsers() {
+        return userRepository.findAll()
+                .stream()
+                .map(user -> new UserDto(user.getId(), user.getName(), user.getEmail()))
+                .toList();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> getUser(@PathVariable Long id) {
+        var user =  userRepository.findById(id).orElse(null);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        var userDto = new UserDto(user.getId(), user.getName(), user.getEmail());
+        return ResponseEntity.ok(userDto);
     }
 }
