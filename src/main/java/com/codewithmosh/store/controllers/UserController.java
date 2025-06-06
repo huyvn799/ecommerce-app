70f58bf763a7efe998ea938d6295f8d789ea03d1
huyvn799 <quochuy2001.kb@gmail.com>
2025-06-06 22:51:09 +0700

    Extracting Query Params on URL

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -5,12 +5,12 @@ import com.codewithmosh.store.entities.User;
 import com.codewithmosh.store.mappers.UserMapper;
 import com.codewithmosh.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
 
 @RestController
 @AllArgsConstructor
@@ -21,8 +21,15 @@ public class UserController {
 
     @GetMapping()
     // method: GET
-    public Iterable<UserDto> getAllUsers() {
-        return userRepository.findAll()
+    public Iterable<UserDto> getAllUsers(
+            // Lưu ý phải có những thuộc tính này khi khai báo param
+            @RequestParam(required = false, defaultValue = "", name = "sort") String sortBy
+    ) {
+        // Nếu trong value của param sort ko phải name hoặc email thì trả về 1 giá trị hợp lệ
+        if (!Set.of("name", "email").contains(sortBy)) {
+            sortBy = "name";
+        }
+        return userRepository.findAll(Sort.by(sortBy))
                 .stream()
                 .map(userMapper::toDto) //cú pháp chuẩn: user -> userMapper.toDto(user
                 .toList();
--- src/main/java/com/codewithmosh/store/dtos/UserDto.java
+++ src/main/java/com/codewithmosh/store/dtos/UserDto.java
@@ -1,12 +1,20 @@
 package com.codewithmosh.store.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @Getter
 public class UserDto {
+    @JsonProperty("user_id") // đổi tên property từ id -> user_id
     private Long id;
     private String name;
     private String email;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") // format lại hiển thị dữ liệu
+    private LocalDateTime createdAt;
 }
