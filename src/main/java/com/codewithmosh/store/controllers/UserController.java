74d4feadce456bf11ee3f91260c1b988b02b4502
huyvn799 <quochuy2001.kb@gmail.com>
2025-06-12 10:53:29 +0700

    Create Action change password

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.codewithmosh.store.controllers;
 
+import com.codewithmosh.store.dtos.ChangePasswordRequest;
 import com.codewithmosh.store.dtos.ResgisterUserRequest;
 import com.codewithmosh.store.dtos.UpdateUserRequest;
 import com.codewithmosh.store.dtos.UserDto;
@@ -8,6 +9,8 @@ import com.codewithmosh.store.mappers.UserMapper;
 import com.codewithmosh.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -84,10 +87,30 @@ public class UserController {
     public ResponseEntity<Void> deleteUser(@PathVariable(name = "id") Long id){
         var user = userRepository.findById(id).orElse(null);
         if (user == null) {
-            ResponseEntity.notFound().build();
+            return ResponseEntity.notFound().build();
         } else {
             userRepository.delete(user);
         }
+        return ResponseEntity.noContent().build();
+    }
+
+    // Change Password
+    @PostMapping("/{id}/change-password")
+    public ResponseEntity<Void> changePassword(
+        @PathVariable(name = "id") Long id,
+        @RequestBody ChangePasswordRequest request
+    ) {
+        var user = userRepository.findById(id).orElse(null);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+        
+        if (!user.getPassword().equals(request.getOldPassword())) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+
+        user.setPassword(request.getNewPassword());
+        userRepository.save(user);
         return ResponseEntity.noContent().build();
     }
 }
